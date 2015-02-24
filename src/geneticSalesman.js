1d5262c1d057dd770b2ac49b5aca4d465210a857
John Markos O'Neill <john@nhoj.com>
2015-02-24 10:24:10 -0800

    Alternative solution by John O'Neill.
    Not exactly a genetic algorithm as described in the problem
    statement, but seems to perform better than a "real" genetic
    algorithm.

--- src/geneticSalesman.js
+++ src/geneticSalesman.js
@@ -1,4 +1,4 @@
-var geneticSalesman = function(genes, assessFitness, initiateBloodline, mutate, availableResources){
+var geneticSalesman = function(genes, assessFitness, initiateBloodline, mutate, availableResources) {
   var options = {
     numberOfBloodlines: 10,
     offspringPerSurvivor: 50,
@@ -6,71 +6,66 @@ var geneticSalesman = function(genes, assessFitness, initiateBloodline, mutate, 
   var generation = 1;
 
   var bloodlines = [];
-  for(var i = 0; i < options.numberOfBloodlines; i++){
-    bloodlines.push(initiateBloodline(genes));
+  for (var i = 0; i < options.numberOfBloodlines; i++) {
+    var newBloodline = initiateBloodline(genes);
+    bloodlines.push(newBloodline);
   }
-
-  while(availableResources){
-    console.log('\n\n\nGeneration:', generation);
-    for(var i = 0; i < bloodlines.length; i++){
-      var survivor = bloodlines[i];
-      var survivorFitness = assessFitness(survivor);
-      for(var offspring = 0; offspring < options.offspringPerSurvivor; offspring++){
-        var currentOffspring = mutate(bloodlines[i]);
-        var currentFitness = assessFitness(currentOffspring);
-        if(currentFitness < survivorFitness){
-          survivor = currentOffspring;
-          survivorFitness = currentFitness;
-        }
+  var mutations = 0;
+  while (availableResources > 0) {
+    var blCopy = bloodlines.slice();
+    for (var blCount = 0; blCount < blCopy.length; blCount++) {
+      var currentRoute = blCopy[blCount];
+      var alternateRoute = alterRoute(currentRoute);
+      if (assessFitness(alternateRoute) < assessFitness(currentRoute)) {
+        bloodlines[blCount] = alternateRoute;
       }
-      console.log(survivorFitness);
-      bloodlines[i] = survivor;
     }
-
-    availableResources--;
-    generation++;
+    mutations++;
+    if (mutations % options.numberOfBloodlines === 0) {
+      availableResources--;
+    }
   }
-
-  bloodlines.sort(function(bloodline1, bloodline2){
-    return assessFitness(bloodline1) - assessFitness(bloodline2);
-  });
-
   var optimalRoute = bloodlines[0];
-  var optimalFitness = assessFitness(bloodlines[0]);
-
-  console.log("optimalRoute", optimalFitness, "meters");
-  console.log('route', optimalRoute);
+  for (var survivorCount = 1; survivorCount < bloodlines.length; survivorCount++) {
+    var survivor = bloodlines[survivorCount];
+    if (assessFitness(survivor) < assessFitness(optimalRoute)) {
+      optimalRoute = survivor;
+    }
+  }
   return optimalRoute;
-}
+};
 
-var createRoute = function(cities){
+var createRoute = function(cities) {
   var route = cities.slice();
-  for(var i = 0; i < route.length; i++){
+  for (var i = 0; i < route.length; i++) {
     var randomIndex = Math.floor(Math.random() * i);
     route[i] = route[randomIndex];
     route[randomIndex] = cities[i];
   }
   return route;
-}
+};
 
-var alterRoute = function(route){
-  var alteredRoute = route.slice();
-  var index1 = Math.floor(Math.random() * route.length)
-  var index2 = Math.floor(Math.random() * route.length)
-  alteredRoute[index1] = alteredRoute[index2];
-  alteredRoute[index2] = route[index1];
-  return alteredRoute;
-}
+var alterRoute = function(route) {
+  var routeCopy = route.slice();
+  var idx1 = Math.floor(Math.random() * routeCopy.length);
+  var tmpCity1 = routeCopy[idx1];
+  var idx2 = idx1;
+  while (idx2 === idx1) {
+    idx2 = Math.floor(Math.random() * routeCopy.length);
+  }
+  routeCopy[idx1] = routeCopy[idx2];
+  routeCopy[idx2] = tmpCity1;
+  return routeCopy;
+};
 
-var calculateDistance = function(route){
-  var distances = route.map(function(city, index, route){
+var calculateDistance = function(route) {
+  var distances = route.map(function(city, index, route) {
     var nextCity = route[index + 1] || route[0];
     var distance = distanceCalculator(city, nextCity);
     return distance;
   });
 
-  return distances.reduce(function(distance1, distance2){
+  return distances.reduce(function(distance1, distance2) {
     return distance1 + distance2;
   });
-}
-
+};
